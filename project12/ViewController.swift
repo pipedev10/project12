cc70df900f7f94b9d59b9fc3b80b676d3143a84b
Felipe Carrasco <felipe.carrasco@codelegends.cl>
2021-09-25 17:19:14 -0300

    Initial Commit

---
+++ project12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project12
+//
+//  Created by Pipe Carrasco on 25-09-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
